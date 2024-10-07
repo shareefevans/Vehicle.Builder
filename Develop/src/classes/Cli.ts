0113c029eeeb301da086505853a460b8455c72f4
shareefevans <shareef3vans@gmail.com>
2024-10-07 13:13:13 +1100

    completed remaining methods within the classes + began the ReadMe file

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -273,7 +273,7 @@ class Cli {
 
   // method to find a vehicle to tow
   // TODO: add a parameter to accept a truck object
-  findVehicleToTow(): void {
+  findVehicleToTow(truck: Truck): void {
     inquirer
       .prompt([
         {
@@ -321,8 +321,8 @@ class Cli {
             'Turn right',
             'Turn left',
             'Reverse',
-            'tow',
-            'wheele',
+            'Tow',
+            'Wheelie',
             'Select or create another vehicle',
             'Exit',
           ],
@@ -386,11 +386,27 @@ class Cli {
               this.vehicles[i].reverse();
             }
           }
-        }
-        // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
-
-        // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
-        else if (answers.action === 'Select or create another vehicle') {
+          // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
+          // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
+        } else if (answers.action === 'Tow') {
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin) {
+              if (this.vehicles[i] instanceof Truck) {
+                this.findVehicleToTow(this.vehicles[i] as Truck);
+              } else {
+                console.log('cant tow vehicle');
+              }
+            }
+          }
+        } else if (answers.action === 'Wheelie') {
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin) {
+              if (this.vehicles[i] instanceof Motorbike) {
+                (this.vehicles[i] as Motorbike).wheelie();
+              } else console.log('Only bikes perform wheelies');
+            }
+          }
+        } else if (answers.action === 'Select or create another vehicle') {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
           this.startCli();
           return;
--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Vehicle.Builder
+# 🏎️ Vehicle.Builder
+
+![Inquirer Version](https://img.shields.io/npm/v/inquirer.svg?label=Inquirer)
+
+## 🚀 Description
+
+### 📋 Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Walkthrough](#walkthrough)
+- [Questions](#questions)
+- [License](#license)
+
+## 🔌 Installation
+
+> [!IMPORTANT]
+> This application will require users to use terminal (Mac) or GitBash (Windows) for it to generate a readMe file/ or the command line in Vs Code for the product to work 🚨.
+
+1. Clone project using `git clone` from this like [Here](https://github.com/shareefevans/Vehicle.Builder)
+2. Open the project folder in a text editor such as `Vs Code` or an alternative editor
+3. Open the command line/terminal/gitbash and navigate to the appropriate folder within Vehicle.Builder labelled `SRC`
+4. Run `npm i` to install an dependencies & then `npm run start`/`npm run build`
+
+## ✋ Usage
+
+1. Open up your text editor
+2. Open up the command line in the text editor or terminal/gitbash
+3. Navigate to the folder that contains the **compiled** application in terminal (the folder labelled `dist` inside the Vehicle.Builder folder)
+4. Type `node index.js` into the terminal
+5. Fill out the prompts as they are presented to you
+6. Once completed, vehicles have been made and both existing and newly created vehciles have been tested, when presented with the action menu, navigate down to `Exit` to terminate the application.
+
+## ➕ Contributing
+
+[Contributor Covenant](https://www.contributor-covenant.org/)
+
+## ✅ Tests
+
+## 📺 Walkthrough
+
+[Follow Walkthrough link]()
+
+## ❓ Questions
+
+Checkout my github profile below 👇
+
+- GitHub Name: shareefevans
+- GitHub Link: [Follow link here](https://github.com/shareefevans)
+
+_If you have any questions, don't hesitate to reach out vie the emial address below_
+*shareef3vans@gmail.com*
+
+## 🏆 License
+
+- Donwloads: ![NPM Downloads](https://img.shields.io/npm/dm/inquirer)
+- License: ![NPM License](https://img.shields.io/npm/l/inquirer)
